4d0960a3ccf1e09dbbe8e812258e96e3648187f9
Taya <justataya@gmail.com>
2020-05-22 13:59:36 -0700

    Add Fermat primality test

---
+++ src/main/java/com/knu/lab1/FermatTest.java
@@ -0,0 +1,25 @@
+package com.knu.lab1;
+
+import java.math.BigInteger;
+
+public class FermatTest {
+    private RandomGenerator generator;
+    private int loopNumber;
+
+    public FermatTest(RandomGenerator generator, int loopNumber) {
+        this.generator = generator;
+        this.loopNumber = loopNumber;
+    }
+
+    public boolean testPrimality(BigInteger value) {
+        BigInteger valueSubtracted = value.subtract(BigInteger.ONE);
+        for (int i = 0; i < loopNumber; i++) {
+            BigInteger randomNumber = generator.getBigInteger(BigInteger.ONE, valueSubtracted);
+            BigInteger testingNumber = BinPow.compute(randomNumber, valueSubtracted, value);
+            if (testingNumber.compareTo(BigInteger.ONE) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/knu/lab1/PrimalityTestTest.java
@@ -0,0 +1,24 @@
+package com.knu.lab1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PrimalityTestTest {
+    @Test
+    public void primesFermatTest() {
+    }
+
+    @Test
+    public void compositesFermatTest() {
+    }
+
+    //TODO: implement RabinTest
+    @Test
+    public void primesRabinTest() {
+    }
+
+    @Test
+    public void compositesRabinTest() {
+    }
+}
